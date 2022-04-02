9f1879a549e2b93c3f39af8175178b67345d7ec2
Jakub Kassak <mojspam.jk@gmail.com>
2022-04-02 12:08:33 +0200

    rule - some game mechanics

--- pharaoh/rule.py
+++ pharaoh/rule.py
@@ -1,11 +1,44 @@
-from __future__ import annotations
+from typing import List, Optional, Tuple, Callable
 
-from typing import List
+from pharaoh.card import Card, Value, Suit
+from pharaoh.move import Move, Action, ChangeVariable
 
-from pharaoh.card import Deck
-from pharaoh.move import Move
+
+def ace_was_played(cards: Tuple[Card]) -> Optional[Action]:
+    if cards[0].value == Value.ACE:
+        return ChangeVariable('ace', lambda a1, a2=(len(cards) + 1): a1 + a2, f'ace += {len(cards) + 1}')
+    return None
+
+
+def leaves_under_as_first(cards: Tuple[Card]) -> Optional[Action]:
+    if cards[0].value == Value.UNDER and cards[0].suit == Suit.LEAF:
+        return ChangeVariable('cnt', lambda _: 1, 'cnt = 1')
+    return None
+
+
+def change_ace_counter(cards: Tuple[Card]) -> Optional[Action]:
+    return ChangeVariable('ace', lambda ace: max(0, ace - 1), 'ace = max(0, ace - 1)')
+
+
+def change_suit_to_top(cards: Tuple[Card]) -> Optional[Action]:
+    if cards[0].value != Value.OVER:
+        return ChangeVariable('suit', lambda _, s=cards[-1].suit: s, f'suit={cards[-1].suit}')
+    return None
+
+
+def change_val_to_top(cards: Tuple[Card]) -> Optional[Action]:
+    return ChangeVariable('suit', lambda _, val=cards[-1].value: val, f'suit={cards[-1].value}')
+
+
+game_mechanics_for_played_cards: List[Callable[[Tuple[Card]], Optional[Action]]] = [
+    ace_was_played,
+    leaves_under_as_first,
+    change_ace_counter,
+    change_suit_to_top,
+    change_val_to_top
+]
 
 
 class Rule:
-    def generate_moves(self, deck: Deck) -> List[Move]:
+    def generate_moves(self, deck: List[Card]) -> List[Move]:
         raise NotImplementedError
