bf35f623374060a93e63bca27efdffc280919ffe
Jakub Kassak <mojspam.jk@gmail.com>
2022-04-10 14:00:47 +0200

    game - functions create_game, legal_moves, finished, winners

--- pharaoh/game.py
+++ pharaoh/game.py
@@ -1,20 +1,31 @@
-from typing import List
+from typing import List, Iterable, Tuple, Optional
 
+from pvectorc import pvector
+from pyrsistent.typing import PVector
+
+from pharaoh.card import Deck
 from pharaoh.game_state import GameState
-from pharaoh.rule import Move
+from pharaoh.rule import Move, Rule
 
 
-def start() -> GameState:
-    raise NotImplementedError
+def create_game(ruleset: Iterable[Rule], deck: Deck, player_count: int) -> Tuple[GameState, PVector[Move]]:
+    moves: List[Move] = []
+    for rule in ruleset:
+        moves.extend(rule.generate_moves(deck, player_count))
+    return GameState.init_state(deck, player_count), pvector(moves)
 
 
-def legal_moves(state: GameState) -> List[Move]:
-    raise NotImplementedError
+def legal_moves(state: GameState, moves: Iterable[Move]) -> List[Move]:
+    return [mv for mv in moves if mv.test(state)]
 
 
-def next_state(state: GameState, rule: Move) -> GameState:
-    raise NotImplementedError
+def finished(state: GameState) -> bool:
+    return 1 == sum(x == -1 for x in state.lp_mc)
 
 
-def winners(state: GameState) -> List[int]:
-    raise NotImplementedError
+def winners(state: GameState) -> Optional[List[int]]:
+    if not finished(state):
+        return None
+    order = list(zip(state.lp_mc, range(1, 10)))
+    order.sort()
+    return [x[1] for x in order]
